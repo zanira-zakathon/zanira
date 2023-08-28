46c8a63c8eb3cec6a23f870d8f01ab76daf2a971
Muhammad Akhsanul Zidan <akhsanul.zidan25@gmail.com>
2023-08-28 21:18:36 +0700

    pengumpulan screen and viewmodel

---
+++ zanira/lib/screen/pengumpulan/pengumpulan_screen.dart
@@ -0,0 +1,18 @@
+//bikin UI nya dulu ya
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class pengumpulanScreen extends ConsumerWidget {
+  //late PengumpulanViewModel pengumpulanViewModel;
+  //late PengumpulanState state;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    //pengumpulanViewModel = ref.watch(pengumpulanVMProvider.notifier);
+    //state = ref.watch(pengumpulanVMProvider);
+    return Scaffold(
+        //pengumpulanViewModel.updateAllData();
+        //Text(state.terkumpul);
+        );
+  }
+}
