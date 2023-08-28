284eb6603a361ee0441a01c52f21fa7d87bcd275
Muhammad Akhsanul Zidan <akhsanul.zidan25@gmail.com>
2023-08-28 21:18:17 +0700

    pendistribusian screen and viewmodel

---
+++ zanira/lib/screen/pendistribusian/pendistribusian_screen.dart
@@ -0,0 +1,18 @@
+//bikin UI nya dulu ya
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class PendistribusianScreen extends ConsumerWidget {
+  //late PendistribusianViewModel pendistribusianViewModel;
+  //late PendistribusianState state;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    //pendistribusianViewModel = ref.watch(PendistribusianVMProvider.notifier);
+    //state = ref.watch(PendistribusianVMProvider);
+    return Scaffold(
+        //pendistribusianViewModel.updateAllData();
+        //Text(state.terkumpul);
+        );
+  }
+}
---
+++ zanira/lib/screen/pendistribusian/pendistribusian_state.dart
@@ -0,0 +1,17 @@
+// import 'package:freezed_annotation/freezed_annotation.dart';
+
+// part 'pendistribusian_state.freezed.dart';
+// part 'pendistribusian_state.g.dart';
+
+// @freezed
+// class PendistribusianState with _$PendistribusianState{
+//   const factory PendistribusianState({
+//     @Default(0) int terkumpul;
+    
+//   })=_PendistribusianState;
+
+//   factory PendistribusianState.fromJson(Map<String, Object?>json)=>_$PendistribusianStateFromJson(json);
+// }
+
+//to run the builder, write on terminal: flutter pub run build_runner build --delete-conflicting-outputs
+
