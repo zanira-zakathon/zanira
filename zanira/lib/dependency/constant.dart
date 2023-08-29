6e4c494e71a17eb9c556874780daefb58487a956
Muhammad Akhsanul Zidan <akhsanul.zidan25@gmail.com>
2023-08-29 12:43:26 +0700

    constant

---
+++ zanira/lib/dependency/constant.dart
@@ -0,0 +1,5 @@
+const baseURL = 'http://192.168.43.104:8080/api';
+const loginURL = baseURL + '/login';
+const signupURL = baseURL + '/signup';
+const logoutURL = baseURL + '/logout';
+const userURL = baseURL + '/user';
