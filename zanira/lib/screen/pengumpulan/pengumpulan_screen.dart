ecc0617f9bda0cb6128a780267047f27efe04476
Raisa <raisa.imani.sani@gmail.com>
2023-08-30 00:11:15 +0700

    -

--- zanira/lib/screen/pengumpulan/pengumpulan_screen.dart
+++ zanira/lib/screen/pengumpulan/pengumpulan_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:zanira/screen/pengumpulan/pengumpulan_state.dart';
 import 'package:zanira/screen/pengumpulan/pengumpulan_viewModel.dart';
+import 'package:zanira/style/color.dart';
 
 class pengumpulanScreen extends ConsumerWidget {
   late PengumpulanViewModel pengumpulanViewModel;
@@ -13,13 +14,26 @@ class pengumpulanScreen extends ConsumerWidget {
     pengumpulanViewModel = ref.watch(PengumpulanVMProvider.notifier);
     state = ref.watch(PengumpulanVMProvider);
     return Scaffold(
-        body: Column(children: [
-      TextButton(
-          onPressed: () {
-            //pengumpulanViewModel.updateAllData();
-          },
-          child: Text('b')),
-      //Text(ref.read(pengumpulanViewModel).state.monthFilter))
-    ]));
+      //Topbar judul dan backbutton
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Align(
+          alignment: Alignment.centerRight,
+          child: Text("Pengumpulan",
+              style: TextStyle(
+                  color: blackprimary,
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold)),
+        ),
+        leadingWidth: 100,
+        leading: Icon(
+          Icons.arrow_circle_left_outlined,
+          color: blackprimary,
+          size: 35,
+        ),
+      ),
+
+      // body: ,
+    );
   }
 }
