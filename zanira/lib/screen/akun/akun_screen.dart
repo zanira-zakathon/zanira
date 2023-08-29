28fc6e3cd24d1f305f791d86dc718832cff131c4
Nisrinarhsy <115068021+Nisrinarhsy@users.noreply.github.com>
2023-08-30 00:53:23 +0700

    -

--- zanira/lib/screen/akun/akun_screen.dart
+++ zanira/lib/screen/akun/akun_screen.dart
@@ -4,11 +4,19 @@ class AkunScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          child: Icon(Icons.person_2_outlined),
-        ),
-      ),
-    );
+        backgroundColor: Colors.white,
+        resizeToAvoidBottomInset: false,
+        // body: Center(
+        //   child: Container(
+        //     child: Icon(Icons.person_2_outlined),
+        //   ),
+        body: Container(
+          child: Column(children: <Widget>[
+            Container(
+              child: Icon(Icons.person_2_outlined),
+            ),
+            Container(child: Text('username'))
+          ]),
+        ));
   }
 }
