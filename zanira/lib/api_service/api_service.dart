2751312db195f58ba64e32260b23e3e27b17d9d3
Muhammad Akhsanul Zidan <akhsanul.zidan25@gmail.com>
2023-08-29 12:40:56 +0700

    api service

--- zanira/lib/api_service/api_service.dart
+++ zanira/lib/api_service/api_service.dart
@@ -11,10 +11,41 @@ class APIService {
 
   Future<Map<String, String>> requestHeaders() async {
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-    String token = sharedPreferences.getString('token')??'';
+    String token = sharedPreferences.getString('token') ?? '';
     return {
       'Content-Type': 'application/json',
       'Authorization': 'Bearer $token',
     };
+  }
+
+  void handleError(int statusCode) {
+    String error = '';
+    switch (statusCode) {
+      case 400:
+        error = "Bad Request";
+        break;
+      case 401:
+        error = "Unauthorized";
+        break;
+      case 403:
+        error = "Invalid credentials";
+        break;
+      case 404:
+        error = "Not Found";
+        break;
+      case 408:
+        error = "Connection Timeout";
+        break;
+      case 500:
+        error = "Internal Server Error";
+        break;
+      default:
+        error = "Error occurred while communicating with server";
+        break;
+    }
+  }
+
+  void handleUncaughtError() {
+    //TODO: showDialog when error;
   }
 }
